3e2e256fefd233326f0696095ea7eea85737bf41
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-06-30 19:51:22 -0400

    Remove uptime from node report.

--- lib/chef/knife/reporter_nodes_cli_report.rb
+++ lib/chef/knife/reporter_nodes_cli_report.rb
@@ -14,12 +14,11 @@ class Chef
       def report_nodes
         nodes = Knife::ReporterHelper.nodes
         hostname_header  = Knife::ReporterHelper.extract_longest_string_from(nodes.map { |node| node.name })+2
-        uptime_header    = Knife::ReporterHelper.extract_longest_string_from(nodes.map { |node| node.uptime })+2
         memory_header    = Knife::ReporterHelper.extract_longest_string_from(nodes.map { |node| (node.memory.total.to_i/1024).to_s })+2
 
-        printf "%-#{hostname_header}s %-#{uptime_header}s %-#{memory_header}s %s\n", "Host Name", "Uptime", "Memory", "Run List"
+        printf "%-#{hostname_header}s %-#{memory_header}s %s\n", "Host Name", "Memory", "Run List"
         nodes.each do |node|
-          printf "%-#{hostname_header}s %-#{uptime_header}s %-#{memory_header}s %s\n", node.name, node.uptime, "#{node.memory.total.to_i/1024}MB", node.run_list
+          printf "%-#{hostname_header}s %-#{memory_header}s %s\n", node.name, "#{node.memory.total.to_i/1024}MB", node.run_list
         end
       end
 
